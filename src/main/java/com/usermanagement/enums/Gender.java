986112942a370a18d315bb048d3dd1f7c6df4c4a
neeljoshi780 <neeljoshi780@gmail.com>
2026-07-23 11:49:56 +0530

    feat: support case-insensitive gender values with custom enum validation

--- src/main/java/com/usermanagement/enums/Gender.java
+++ src/main/java/com/usermanagement/enums/Gender.java
@@ -1,5 +1,8 @@
 package com.usermanagement.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.usermanagement.common.exception.BadRequestException;
+
 /**
  * Enum representing user gender.
  */
@@ -7,6 +10,28 @@ public enum Gender {
 
 	MALE,
 	FEMALE,
-	OTHER
+	OTHER;
+
+	/**
+	 * Creates a Gender enum from a case-insensitive value.
+	 *
+	 * @param value gender value
+	 * @return matching Gender enum
+	 * @throws IllegalArgumentException if value is invalid
+	 */
+	@JsonCreator
+	public static Gender fromValue(String value) {
+		if (value == null || value.isBlank()) {
+			return null;
+		}
+		for (Gender gender : values()) {
+			if (gender.name().equalsIgnoreCase(value)) {
+				return gender;
+			}
+		}
+		throw new BadRequestException(
+				"Invalid gender. Allowed values are: MALE, FEMALE, OTHER."
+		);
+	}
 
 }
